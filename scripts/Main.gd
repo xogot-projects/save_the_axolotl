25c8da50acb527ebbc30da39a7d290b5a29df631
Nicolas St-Amour <nstamour.dev@outlook.com>
2025-11-30 18:49:21 -0500

    Add virtual joystick plugin configuration and preview
    - Added Virtual Joystick Add-on
    - Updated Main.tscn and Main.gd to support new Joystick methods

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -3,8 +3,22 @@ extends Node
 # Create and export the mob_scene to add later using the scope
 @export var mob_scene: PackedScene
 
+# We use onready to assign the joystick var before _ready() is called
+@onready var virtual_joystick_left = $"UI/Virtual Joystick Left"
+
 # Create a score variable for score tracking
 var score
+
+
+func _ready() -> void:
+    # Set the Joystick input actions to use.
+    virtual_joystick_left.action_left = "move_left"
+    virtual_joystick_left.action_right = "move_right"
+    virtual_joystick_left.action_up = "move_up"
+    virtual_joystick_left.action_down = "move_down"
+    
+    # Hide the UI by default
+    $UI.hide()
 
 
 # Called when the player dies, performs clean-up actions
@@ -21,6 +35,9 @@ func game_over() -> void:
     
     # Play the death sound
     $DeathSound.play()
+    
+    # Hide the joystick UI
+    $UI.hide()
 
 
 # This will be called when the player presses the start button
@@ -39,6 +56,9 @@ func new_game():
     
     # Show a temporary message of "Get Ready"
     $HUD.show_message("Get Ready")
+    
+    # Show the joystick
+    $UI.show()
 
     # Clear any mobs that might still be on screen
     get_tree().call_group("mobs", "queue_free")
